c9699242df6eed48c5ae58a263f7239e5e6c18cd
MuathAlawi Alawi <muathalawi1@gmail.com>
2018-10-19 00:43:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,9 +24,9 @@ function switchOnCharmFromTip(tip) {
   switch(tip) {
     
     case 'generous':
-    
+      return 'Thank you so much.'
     case 'not as generous':
-      
+      return
     case 'thanks for everything':
   }
   
