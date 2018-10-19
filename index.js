c7679421c8d1ef63ed1247d0097e90cdd0ad79ab
MuathAlawi Alawi <muathalawi1@gmail.com>
2018-10-19 00:32:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 function scuberGreetingForFeet(numberOfFeet) {
   if (numberOfFeet <= 400) {
-    return 'This one is one me!'
+    return 'This one is on me!'
   } else if (numberOfFeet > 2000) {
     return 'I will gladly take your thirty bucks.'
   }
